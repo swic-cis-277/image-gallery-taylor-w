1e722fb68477b16623b64d80aa748c740fa607cf
taylor-w <taylorwood39@gmail.com>
2020-10-25 14:14:21 -0500

    Add script to 'move' 'clicked' image to display img

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-const currentImgAttrs = {};
-
-const display = document.querySelector("#display img");
+const displayImg = document.querySelector("#display img");
 const fsImages = document.querySelectorAll("#film-strip img");
+
+fsImages.forEach(function (fsImg) {
+  fsImg.addEventListener("click", function (event) {
+    displayImg.src = event.target.src;
+    displayImg.alt = event.target.alt;
+  });
+});
